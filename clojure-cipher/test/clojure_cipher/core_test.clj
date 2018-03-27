1390e32932e3eebb5af766dd62a732a17297c9a6
Jordan Biserkov <biserkov@gmail.com>
2018-03-28 02:17:43 +0300

    Fix generators by using gen/char-alpha.

--- clojure-cipher/test/clojure_cipher/core_test.clj
+++ clojure-cipher/test/clojure_cipher/core_test.clj
@@ -1,12 +1,12 @@
 (ns clojure-cipher.core-test
   (:require [clojure.test :refer :all]
-            [clojure-cipher.core :refer :all]
+            [clojure-cipher.core :refer [encode decode]]
             [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
 (clojure.test.check.clojure-test/defspec encode-decode 1000
-  (prop/for-all [message (filter #(< (int \a) (int (Character/toLowerCase %)) (int \z)) (gen/not-empty gen/string-alphanumeric))
-                 secret (filter #(< (int \a) (int (Character/toLowerCase %)) (int \z)) (gen/not-empty gen/string-alphanumeric))]
-                (= (decode (encode message secret) secret) message)))
-
+  (prop/for-all [message (gen/list-distinct gen/char-alpha {:min-elements 2})
+                 secret (gen/list-distinct gen/char-alpha {:min-elements 2})]
+                (= (decode (encode message secret) secret)
+                   (apply str (map #(Character/toLowerCase %1) message)))))
