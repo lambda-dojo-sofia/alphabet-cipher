17ee0a5cabe0ebcc6d98f6fb48561cf59e34c89e
Albert Peschar <albert@peschar.net>
2018-03-27 21:16:17 +0300

    Clean up

--- ocaml/cypher.ml
+++ ocaml/cypher.ml
@@ -1,22 +1,20 @@
-let keyword = "vigilance"
-let plaintext = "meetmeontuesdayeveningatseven"
-let encrypted = "hmkbxebpxpmyllyrxiiqtoltfgzzv"
-
 let findChar keyword i = keyword.[i mod String.length keyword]
 
-let intToLetter i = Char.chr (i + Char.code 'a')
-let letterToInt l = Char.code l - Char.code 'a'
 let wrapAround n = match n with
   | _ when n < 0 -> (n + 26)
   | _ -> n mod 26
 
-let shiftChar direction a b = intToLetter (wrapAround ((letterToInt a) * direction + (letterToInt b)))
+let intToLetter i = Char.chr ((wrapAround i) + Char.code 'a')
+let letterToInt l = Char.code l - Char.code 'a'
+
+let shiftChar direction a b =
+        intToLetter ((letterToInt a) * direction + (letterToInt b))
 
 let shift direction keyword i c = shiftChar direction (findChar keyword i) c
 
 let cryptomagic fn keyword plaintext = String.mapi (fn keyword) plaintext
 
-let encrypt = cryptomagic (shift 1)
+let encrypt = cryptomagic (shift   1 )
 let decrypt = cryptomagic (shift (-1))
 
 let prompt message =
@@ -25,10 +23,10 @@ let prompt message =
 
 let getMode = match (prompt "Mode? [e|d] ") with
     | "e" -> encrypt
-    | _ -> decrypt
+    | _   -> decrypt
 
 let () =
     let fn = getMode in
     let keyword = prompt "Keyword? " in
-    let plaintext = prompt "Plain text? " in
-    print_endline (fn keyword plaintext)
+    let message = prompt "Message? " in
+    print_endline (fn keyword message)
