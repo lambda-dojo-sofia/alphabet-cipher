8320a7eb24a8dffeb1e1d022139791eeb86421d3
Evgeni Kolev <evgenysw@gmail.com>
2018-03-27 21:35:58 +0300

    Update clojure version

--- clojure-cipher/test/clojure_cipher/core_test.clj
+++ clojure-cipher/test/clojure_cipher/core_test.clj
@@ -1,6 +1,23 @@
 (ns clojure-cipher.core-test
   (:require [clojure.test :refer :all]
-            [clojure-cipher.core :refer :all]))
+            [clojure-cipher.core :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [com.gfredericks.test.chuck.properties :as prop']))
+
+(clojure.test.check.clojure-test/defspec encode-decode 1000
+  (prop/for-all [message (filter #(< (int \a) (int (Character/toLowerCase %)) (int \z)) (gen/not-empty gen/string-alphanumeric))
+                 secret (filter #(< (int \a) (int (Character/toLowerCase %)) (int \z)) (gen/not-empty gen/string-alphanumeric))]
+                (= (decode (encode message secret) secret) message)))
+
+;; (clojure.test.check.clojure-test/defspec encode-decode 2
+;;   (prop'/for-all [message (gen/string-alphanumeric)
+;;                  secret (gen/string-alphanumeric)]
+;;                 (let [m (filter #(< (int \a) (int (Character/toLowerCase %)) (int \z)) message)
+;;                       s "fake"]
+;;                   (= (decode (encode m s) s) m))))
+
 
 (deftest a-test
   (testing "FIXME, I fail."
