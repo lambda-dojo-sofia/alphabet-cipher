5f1bb814f131cebafbeefcb1167139c073db69a1
Jordan Biserkov <biserkov@gmail.com>
2018-03-28 02:25:25 +0300

    Increase number of tests by an order of magnitude. Takes 10 secs on tablet.

--- clojure-cipher/test/clojure_cipher/core_test.clj
+++ clojure-cipher/test/clojure_cipher/core_test.clj
@@ -5,7 +5,7 @@
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
-(clojure.test.check.clojure-test/defspec encode-decode 1000
+(clojure.test.check.clojure-test/defspec encode-decode 10000
   (prop/for-all [message (gen/list-distinct gen/char-alpha {:min-elements 2})
                  secret (gen/list-distinct gen/char-alpha {:min-elements 2})]
                 (= (decode (encode message secret) secret)
