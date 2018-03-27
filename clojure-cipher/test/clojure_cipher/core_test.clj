1b3e7da3907caf98c3ef541cd09ead87ba922bdb
Jordan Biserkov <biserkov@gmail.com>
2018-03-28 02:27:32 +0300

    Properly require defspec.

--- clojure-cipher/test/clojure_cipher/core_test.clj
+++ clojure-cipher/test/clojure_cipher/core_test.clj
@@ -1,11 +1,11 @@
 (ns clojure-cipher.core-test
   (:require [clojure.test :refer :all]
             [clojure-cipher.core :refer [encode decode]]
-            [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
-(clojure.test.check.clojure-test/defspec encode-decode 10000
+(defspec encode-decode 10000
   (prop/for-all [message (gen/list-distinct gen/char-alpha {:min-elements 2})
                  secret (gen/list-distinct gen/char-alpha {:min-elements 2})]
                 (= (decode (encode message secret) secret)
