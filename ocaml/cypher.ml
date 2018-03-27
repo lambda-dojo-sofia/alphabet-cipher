75f25e628f0e0082bf217a63727ce6754a15ba39
Albert Peschar <albert@peschar.net>
2018-03-27 19:53:11 +0300

    WIP: OCaml

--- ocaml/cypher.ml
+++ ocaml/cypher.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!" ;;
+let keyword = "vigilance" ;;
+let plaintext = "meetmeontuesdayeveningatseven" ;;
+
+let findChar keyword i = keyword.[i mod String.length keyword] ;;
+
+let intToLetter i = Char.chr (i + Char.code 'a')
+let letterToInt l = Char.code l - Char.code 'a'
+let shiftChar a b = intToLetter ((letterToInt a) + (letterToInt b)) mod 26
+
+let shift keyword i c = shiftChar (findChar keyword i) c ;;
+
+let encrypt keyword plaintext = String.mapi (shift keyword) plaintext ;;
+
+let () = print_endline (encrypt keyword plaintext) ;;
